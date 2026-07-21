0014eb9ee3c7b17386f752dd85f7651c2f028ada
lnoxsian <lnox122224@tutamail.com>
2026-07-22 04:42:36 +0530

    cleanup: removed the scratch dir

--- scratch/merman_test/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let content = "gantt\nMPU EMG Flex : 1d";
-    match merman::parse(content) {
-        Ok(_) => println!("Parsed OK"),
-        Err(e) => println!("Error: {}", e),
-    }
-}
